19a6c644e9b1d02ab8c5f3ba6f3b18efb57da3ce
dajeongkimm <rlaekwjd6566@gmail.com>
2024-07-31 15:40:39 +0900

    add 0730

--- dajeongkimm/0730/Main.java
+++ dajeongkimm/0730/Main.java
@@ -1,97 +1,119 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 class Main {
-    static int N,M,K;
-    static Map<Integer, ArrayList<Integer>> order;
-    static int[] time;
-    static ArrayList<int[]> dfsList;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        time = new int[N+1];
-        st = new StringTokenizer(br.readLine());
-        for (int i=1;i<=N;i++){
-            time[i] = Integer.parseInt(st.nextToken());
-        }
-//        Map<Integer, ArrayList<Integer>> level = new HashMap();
-        order = new HashMap<>();
-        //방향 그래프 (일의 순서)
-        for (int i=0;i<M;i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            order.putIfAbsent(a, new ArrayList<>());
-            order.get(a).add(b);
-        }
-
-        //N-2개 중에서 (2~N-1의 tast 중에서 K개를 0으로 만들기)
+	static int N, M, K;
+	static int[] time;
+	static List<List<Integer>> graph;
+	static int[] inDegree;
+	static ArrayList<int[]> dfsList;
+	static int start, end;
+	static int answer;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		time = new int[N+1];
+		inDegree = new int[N+1];
+		st = new StringTokenizer(br.readLine());
+		for (int i=1;i<=N;i++) {
+			time[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		graph = new ArrayList<>();
+		//그래프 초기화
+		for (int i=0;i<=N;i++) {
+			graph.add(new ArrayList<>());
+		}
+		
+		for (int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph.get(a).add(b);
+			inDegree[b]++;
+		}
+		
+		start = 1;
+		int maxDegree = Arrays.stream(inDegree).max().getAsInt();
+		for (int i=0;i<=N;i++) {
+			if (inDegree[i] == maxDegree) {
+				end = i;
+			}
+		}
+		
+		//N-2개 중에서 (2~N-1의 tast 중에서 K개를 0으로 만들기)
         dfsList = new ArrayList<>();
-        ArrayList<Integer> tmp = new ArrayList<>();
-        DFS(tmp,2);
-
-        int answer = Integer.MAX_VALUE;
-        for (int i=0;i<dfsList.size();i++){
-            int[] k_list = dfsList.get(i);
-            int[] new_time = new int[N+1];
-            for (int j=0;j<N+1;j++){
-                new_time[j] = time[j];
-            }
-            for (int j=0;j<k_list.length;j++){
-                new_time[k_list[j]] = 0;
-            }
-            int tmp_time = BFS(new_time);
-            answer = Math.min(answer, tmp_time);
+        
+        for (int i=2;i<=N;i++) {
+        	ArrayList<Integer> tmp = new ArrayList<>();
+        	DFS(tmp,i);
+        }
+        
+        
+        answer = Integer.MAX_VALUE;
+        
+        for (int i=0;i<dfsList.size();i++) {
+        	int tmpTime = 0;
+        	int[] zeroTime = dfsList.get(i);
+        	List<Integer> list = Arrays.stream(zeroTime).boxed().collect(Collectors.toList());
+        	Set<Integer> set = new HashSet<>(list);
+        	ArrayDeque<Integer> queue = new ArrayDeque<>();
+        	queue.add(start);
+        	
+        	while(!queue.isEmpty()) {
+        		int cur = queue.poll();
+        		
+        		int addTime = 0;
+        		for (int n : graph.get(cur)) {
+        			inDegree[n]--;
+        			if (inDegree[n] == 0) {
+        				if (!set.contains(n)) {
+        					addTime = Math.max(addTime, time[n]);
+            				queue.add(n);
+        				}
+        				
+        			}
+        		}
+        		tmpTime += addTime;
+        	}
+        	System.out.println("tmpTime");
+        	System.out.println(tmpTime);
+        	answer = Math.min(answer, tmpTime);
+        	
         }
         System.out.println(answer);
-    }
-    private static void DFS(ArrayList<Integer> cur_list, int idx){
-        if (cur_list.size() == K){
-            int[] result = new int[cur_list.size()];
-            for (int i=0;i<cur_list.size();i++) {
-                result[i] = cur_list.get(i);
-            }
-            dfsList.add(result);
-        }
-        for (int i=idx;i<=N-1;i++){
-            cur_list.add(i);
-            DFS(cur_list,i+1);
-            cur_list.remove(cur_list.size()-1);
-        }
-    }
-    private static int BFS(int[] new_time){
-        ArrayDeque<Integer> queue = new ArrayDeque<>();
-        queue.add(1);
-        boolean[] visited = new boolean[N+1];
-        visited[1] = true;
-        int answer = new_time[1];
-//        visited[N] = true;
-        while (!queue.isEmpty()){
-            int cur = queue.poll();
-            int tmp_time = -1;
-            for (Integer n : order.getOrDefault(cur, new ArrayList<>())) {
-                int num = n;
-                if (!visited[num]) {
-                    tmp_time = Math.max(tmp_time, new_time[num]);
-                    visited[num] = true;
-                    queue.add(num);
-                }
-            }
-//            System.out.println(tmp_time);
-            if (tmp_time != -1){
-                answer += tmp_time;
-            }
-//            System.out.println(answer);
-        }
-        return answer;
-    }
+        
+        
+	}
+	private static void DFS(ArrayList<Integer> cur_list, int idx) {
+		if (cur_list.size() == K) {
+			int[] result = new int[cur_list.size()];
+			for (int i=0;i<cur_list.size();i++) {
+				result[i] = cur_list.get(i);
+			}
+			dfsList.add(result);
+		}
+		
+		for (int i=idx;i<=N;i++) {
+			if (i!=end) {
+				cur_list.add(i);
+				DFS(cur_list, i+1);
+				cur_list.remove(cur_list.size()-1);
+			}
+		}
+	}
 }
