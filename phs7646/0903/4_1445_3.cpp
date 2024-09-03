6fb54c616d11864574dc4d16f48287d1d1c7062e
Hanseong <phs7646@kaist.ac.kr>
2024-09-03 22:19:50 +0900

    0903 hanseong

---
+++ phs7646/0903/4_1445_3.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<string>
+using namespace std;
+typedef pair<int,int> pp;
+pp moves[] = {{1,0},{-1,0},{0,1},{0,-1}};
+
+int sy,sx;
+int fy,fx;
+int map[50][50]; //쓰레기 : 2 , 쓰레기 옆칸 : 1, 빈칸 : 0
+pp visited[50][50];
+
+int main() {
+    //input
+    int N,M; cin >> N >> M;
+    for(int i = 0;i < N;i++) {
+        for(int j = 0;j < M;j++) {
+            char c; cin >> c;
+            if(c == '.') continue;
+            if(c == 'g') {
+                map[i][j] = 2;
+                for(pp move : moves) {
+                    int a = i + move.first;
+                    int b = j + move.second;
+                    if(0 <= a && a < N && 0 <= b && b < M && map[a][b] == 0) {
+                        map[a][b] = 1;
+                    }
+                }
+            } else if(c == 'F') {
+                fy = i; fx = j;
+            } else if(c == 'S') {
+                sy = i; sx = j;
+            }
+        }
+    }
+
+    //initialize visited
+    for(int i = 0;i < N;i++) {
+        for(int j = 0;j < M;j++) {
+            visited[i][j] = {-1,-1};
+        }
+    }
+
+    //logic 1 : bfs
+    queue<pp> q;
+    pp start = {sy,sx};
+    q.emplace(sy,sx);
+    visited[sy][sx] = {0,map[sy][sx]};
+    int answer1 = 1e9;
+    int answer2 = 1e9;
+    while(!q.empty()) {
+        pp pos = q.front(); q.pop();
+        pp cos = visited[pos.first][pos.second];
+
+        for(pp move : moves) {
+            int ni = pos.first + move.first;
+            int nj = pos.second + move.second;
+            if(0 > ni || ni >= N || 0 > nj || nj >= M) continue;
+            pp newcos = {cos.first,cos.second};
+            if(map[ni][nj] == 2) newcos = {cos.first+1,cos.second};
+            else if(map[ni][nj] == 1) newcos = {cos.first,cos.second+1};
+
+            if(ni == fy && nj == fx) {
+                //update, no propagate
+                if(newcos.first < answer1) {
+                    answer1 = newcos.first;
+                    answer2 = newcos.second;
+                } else if(newcos.first == answer1 && newcos.second < answer2) {
+                    answer2 = newcos.second;
+                }
+                continue;
+            }
+
+            //can newcos visit the npos?
+            bool flag = true;
+            pp record = visited[ni][nj];
+            if(record.first == -1 || newcos.first < record.first ||
+                (newcos.first==record.first && newcos.second < record.second)) {
+                visited[ni][nj] = newcos;
+                q.emplace(ni,nj);
+            }
+        }
+    }
+    if(map[sy][sx] == 1) answer2--;
+    if(map[fy][fx] == 1) answer2--;
+    cout << answer1 << " " << answer2;
+    return 0;
+}
